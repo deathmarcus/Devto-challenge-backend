dccdb6e2eb15e883b012f559e630173de16efa21
Marco Castañeda <marco.dcc@gmail.com>
2022-08-26 20:23:46 -0500

    Feat: Auth middleware

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,18 @@
+const jwt = require("../lib/jwt.lib")
+
+const auth = (request, response, next) => {
+    try {
+        const authorization = request.headers.authorization || ""
+        const token = authorization.replace("Bearer ", "")
+        const verifiedToken = jwt.verify(token)
+        next()
+    }catch(error) {
+        response.status(401)
+        response.json({
+            success: false,
+            error: error.message
+        })
+    }
+}
+
+module.exports = auth
