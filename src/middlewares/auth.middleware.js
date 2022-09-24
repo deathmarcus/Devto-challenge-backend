9d0267a220e45d9980ae8e180fef57dda1acd366
Marco Castañeda <marco.dcc@gmail.com>
2022-09-23 22:26:23 -0500

    Feat: verifyUser

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,41 +1,62 @@
-const jwt = require("../lib/jwt.lib")
-const createError = require("http-errors")
-const { getPost } = require("../usecases/post.usecase")
+const jwt = require("../lib/jwt.lib");
+const createError = require("http-errors");
+const { getPost } = require("../usecases/post.usecase");
+const { getUser } = require("../usecases/user.usecase");
 
 const auth = (request, response, next) => {
-    try {
-        const authorization = request.headers.authorization || ""
-        const token = authorization.replace("Bearer ", "")
-        const verifiedToken = jwt.verify(token)
-        next()
-    }catch(error) {
-        response.status(401)
-        response.json({
-            success: false,
-            error: error.message
-        })
-    }
-}
+  try {
+    const authorization = request.headers.authorization || "";
+    const token = authorization.replace("Bearer ", "");
+    const verifiedToken = jwt.verify(token);
+    next();
+  } catch (error) {
+    response.status(401);
+    response.json({
+      success: false,
+      error: error.message,
+    });
+  }
+};
 
 const verifyOwner = async (request, response, next) => {
-    try {
-        const authorization = request.headers.authorization || ""
-        const token = authorization.replace("Bearer ", "")
-        const verifiedOwner = jwt.verify(token)
-        const postUserId = await getPost(request.params.id)
-        const { postAuthorId } = postUserId
-        if(verifiedOwner.id === postAuthorId){
-            next()
-        }else{
-            throw createError(401, "No eres el creador del post")
-        }
-    }catch(error) {
-        response.status(401)
-        response.json({
-            success: false,
-            error: error.message
-        })
+  try {
+    const authorization = request.headers.authorization || "";
+    const token = authorization.replace("Bearer ", "");
+    const verifiedOwner = jwt.verify(token);
+    const postUserId = await getPost(request.params.id);
+    const { postAuthorId } = postUserId;
+    if (verifiedOwner.id === postAuthorId) {
+      next();
+    } else {
+      throw createError(401, "No eres el creador del post");
     }
-}
+  } catch (error) {
+    response.status(401);
+    response.json({
+      success: false,
+      error: error.message,
+    });
+  }
+};
 
-module.exports = {auth, verifyOwner}
+const verifyUser = async (request, response, next) => {
+  try {
+    const authorization = request.headers.authorization || "";
+    const token = authorization.replace("Bearer ", "");
+    const verifiedUser = jwt.verify(token);
+    const userId = await getUser(request.params.id);
+    if (verifiedUser.id === userId) {
+      next();
+    } else {
+      throw createError(401, "No autorizado");
+    }
+  } catch (error) {
+    response.status(401);
+    response.json({
+      success: false,
+      error: error.message,
+    });
+  }
+};
+
+module.exports = { auth, verifyOwner, verifyUser };
